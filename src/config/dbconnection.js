01a1db1c2a4243fbb939e3379155fc0237978dd5
keshavkhetan123 <115499327+keshavkhetan123@users.noreply.github.com>
2024-07-23 17:12:35 +0530

    fixed bugs

--- src/config/dbconnection.js
+++ src/config/dbconnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDb = async () => {
     try{
-        const connect = await mongoose.connect('mongodb+srv://iec2021066:fPQoLJ8B9YHDH0DI@mycluster.5vyhkoy.mongodb.net/paste-backend?retryWrites=true&w=majority')
+        const connect = await mongoose.connect('mongodb+srv://iit2022193:2G151paGc4jMfB8F@cluster0.h057kbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log("Database Connected: ", connect.connection.host, connect.connection.name)
     } catch(err) {
         console.log(err);
